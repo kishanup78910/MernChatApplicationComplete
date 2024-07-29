ee51ff84664af1c89887badaf5345cfc0bfb1e24
Kishan Upadhyay <79304976+kishanup78910@users.noreply.github.com>
2024-07-29 10:11:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-    origin: 'https://mernchatapplicationcomplete.onrender.com',
+    origin: 'https://chatfrontend-bqi6.onrender.com',
     credentials: true
 };
 app.use(cors(corsOption));
